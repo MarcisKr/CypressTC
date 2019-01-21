49b2f988ebb9e8562f5f1bb8d5702afea3d3fe5e
MarcisKr <marcis.vitols@testdevlab.com>
2019-01-21 16:21:16 +0200

    Added few more cases and fixed code style

---
+++ cypress/integration/developerPage/forms.spec.js
@@ -0,0 +1,78 @@
+const page = "https://developer.truecaller.com";
+
+beforeEach(function () {
+    cy
+        .visit(page)
+})
+
+describe('Sign up form', () => {
+    it('Form fields validation', () => {
+        cy
+            .get('.menu > [href="/sign-up"]')
+            .click()
+        cy
+            .get('.text-3xl')
+            .should('be.visible')
+            .and('have.text', 'Sign up')
+        cy
+            .get('#email')
+            .should('be.visible')
+        cy
+            .get('#password')
+            .should('be.visible')
+        cy
+            .get('#passwordConfirm')
+            .should('be.visible')
+        cy
+            .get('#name')
+            .should('be.visible')
+        cy
+            .get('#companyName')
+            .should('be.visible')
+        cy
+            .get('#country')
+            .should('be.visible')
+        cy
+            .get('#accept')
+            .should('be.visible')
+        cy
+            .get('.submit')
+            .should('be.visible')
+            .and('be.disabled')
+            .and('have.text', 'Register')
+    })
+})
+
+describe('Login form', () => {
+    it('Form fields validation', () => {
+        cy
+            .get('.menu > [href="/login"]')
+            .click()
+        cy
+            .get('.text-3xl')
+            .should('be.visible')
+            .and('have.text', 'Login')
+        cy
+            .get('#username')
+            .should('be.visible')
+        cy
+            .get('#password')
+            .should('be.visible')
+        cy
+            .get('.submit')
+            .should('be.visible')
+            .and('be.disabled')
+            .and('have.text', 'Login')
+    })
+
+    it('Open password reset link', () => {
+        cy
+            .visit('https://developer.truecaller.com/login')
+        cy
+            .get('.flex > .link')
+            .click()
+        cy
+            .url()
+            .should('eq', page + '/forgot-password')
+    })
+})
