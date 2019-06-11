293ad9a130100330f6fbdab1616e54c075385781
MarcisKr <marcis.vitols@testdevlab.com>
2019-06-11 10:09:12 +0300

    Feature buttons are wrapped with automation tests

--- cypress/elements/pages/newHomePage/homePage.js
+++ cypress/elements/pages/newHomePage/homePage.js
@@ -18,6 +18,26 @@ class homePage{
     buttonFeaturesDownload(){
         return cy.get('.btn');
     }
+
+    buttonCallerID(){
+        return cy.get('[aria-label="Go to caller id feature"] > svg');
+    }
+
+    buttonSpamProtection(){
+        return cy.get('[aria-label="Go to spam protection feature"] > svg');
+    }
+
+    buttonDialer(){
+        return cy.get('[aria-label="Go to dialer feature"] > svg');
+    }
+
+    buttonMessaging(){
+        return cy.get('[aria-label="Go to messaging feature"] > svg');
+    }
+
+    buttonPay(){
+        return cy.get('[aria-label="Go to pay feature"] > svg');
+    }
 //===================================  METHODS  =======================================
     visitURL(url = "https://www.truecaller.com"){
         cy.visit(url);
@@ -36,6 +56,28 @@ class homePage{
         cy.get('[href="https://play.google.com/store/apps/details?id=com.truecaller&referrer=utm_source%3Dtcweb&rdid=com.truecaller"] > picture > img').should('be.visible');
         cy.get('[href="https://itunes.apple.com/app/apple-store/id448142450?pt=303680&ct=Website1&mt=8"] > picture > img').should('be.visible');
         cy.get('[href="https://apk.truecaller.com/truecaller.apk"] > picture > img').should('be.visible');
+    }
+
+    validateFeatureText(button){
+        const label = cy.get('.md\\:flex > div > .text-3xl');
+        if(button === "Caller ID"){
+            label.should('have.text', 'Caller ID');
+        }
+        else if(button === "Spam Blocking"){
+            label.should('have.text', 'Spam Blocking');
+        }
+        else if(button === "Intelligent dialer"){
+            label.should('have.text', 'Intelligent dialer');
+        }
+        else if(button === "Chat, SMS and Calls"){
+            label.should('have.text', 'Chat, SMS and Calls');
+        }
+        else if(button === "Banking"){
+            label.should('have.text', 'Banking');
+        }
+        else{
+            err;
+        }
     }
 }
 
